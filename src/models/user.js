e5252cbf68f22c29f1cd29019ec2a45475e494a5
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-11-21 15:24:57 +0700

    feat: associations user with role

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(models.Role, {foreignKey: 'role_code', targetKey: 'code', as: 'roleData'})
     }
   }
   User.init({
